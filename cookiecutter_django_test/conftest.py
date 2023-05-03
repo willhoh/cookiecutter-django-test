4092d698f6fcebc478db323007e0fef8da0bdfb4
Willi Hohenstein <willi.hohenstein@seburai.de>
2023-05-03 16:47:16 +0200

    initial commit

---
+++ cookiecutter_django_test/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from cookiecutter_django_test.users.models import User
+from cookiecutter_django_test.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ cookiecutter_django_test/users/tests/test_models.py
@@ -0,0 +1,5 @@
+from cookiecutter_django_test.users.models import User
+
+
+def test_user_get_absolute_url(user: User):
+    assert user.get_absolute_url() == f"/users/{user.pk}/"
